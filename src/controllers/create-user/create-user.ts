6635494777b3bf003b6fbeecc6fe738309bba7db
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-09-16 01:55:06 -0300

    feat: add MongoCreateUserRepository

---
+++ src/controllers/create-user/create-user.ts
@@ -0,0 +1,54 @@
+import { User } from "../../models/user";
+import { HttpResponse, HttpRequest} from "../protocols";
+import {
+  CreateUserParams,
+  ICreateUserController,
+  ICreateUserRepository,
+} from "./protocols";
+
+import validator from "validator";
+
+export class CreateUserController implements ICreateUserController {
+  constructor(private readonly createUserRepository: ICreateUserRepository) {}
+  async handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User>> {
+    try {
+      // verificar campos obrigatórios
+      const RequiredFields = ["firstName", "lastName", "email", "password"];
+
+      // validar se body existe
+      for (const field of RequiredFields) {
+        if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
+          return {
+            statusCode: 400,
+            body: `Field ${field} is required`,
+          };
+        }
+      }
+
+      // verificar se o e-mail é valido
+      const emailIsValid = validator.isEmail(httpRequest.body!.email);
+      if (!emailIsValid) {
+        return {
+          statusCode: 400,
+          body: "E-mail is invalid",
+        };
+      }
+      if (!httpRequest.body) {
+        return {
+          statusCode: 400,
+          body: "Please specify a body",
+        };
+      }
+
+      const user = await this.createUserRepository.createUser(httpRequest.body);
+      return {
+        statusCode: 201,
+        body: user,
+      };
+    } catch (error) {
+      return { statusCode: 500, body: "Something went wrong" };
+    }
+  }
+}
---
+++ src/controllers/create-user/protocols.ts
@@ -0,0 +1,19 @@
+import { User } from "../../models/user";
+import { HttpResponse, httpRequest } from "../protocols";
+
+export interface ICreateUserController {
+  handle(
+    httpRequest: httpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User>>;
+}
+
+export interface CreateUserParams {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export interface ICreateUserRepository {
+  createUser(params: CreateUserParams): Promise<User>;
+}
